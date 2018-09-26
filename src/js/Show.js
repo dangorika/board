1a8c49e193162e4738a243fa493307d9b018f298
dangorika <olha.yev@gmail.com>
2018-09-26 13:28:02 +0300

    add fixes

--- src/js/Show.js
+++ src/js/Show.js
@@ -2,12 +2,21 @@ export default class Show {
   constructor(el) {
     this.show = $(el);
     this.button = this.show.find('.js-s-button');
+    this.content = this.show.find('.js-s-content');
+    this.height = this.content.height();
     this.currentText = this.button.html();
     this._init();
   }
 
   _init() {
+    this._close();
     this.button.on('click', this._toggle.bind(this));
+  }
+
+  _close() {
+    this.content.animate({
+      height: 592
+    });
   }
 
   _toggle(e) {
@@ -17,7 +26,16 @@ export default class Show {
     this.targetS = this.targetButton.closest('.js-s').find('.js-s-content');
     this.targetS.toggleClass('is-open');
     this.newText = this.targetButton.data('change-text');
-    console.log(this.newText);
-    this.targetS.hasClass('is-open') ? this.targetButton.html(this.newText) : this.targetButton.html(this.currentText);
+    if (this.targetS.hasClass('is-open')) {
+      this.targetButton.html(this.newText);
+      this.targetS.animate({
+        height: this.height
+      }, 300);
+    } else {
+      this.targetButton.html(this.currentText);
+      this.targetS.animate({
+        height: 592
+      }, 300);
+    }
   }
 }
